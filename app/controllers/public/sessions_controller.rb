5a003d4fda3ba3e2034aaf1dc96244becc0f68a2
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-04-13 04:39:08 +0000

    customer-log-in

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Public::SessionsController < Devise::SessionsController
-  before_action :customer_state, only: [:create]
+
 
   # GET /resource/sign_in
   # def new
@@ -18,15 +18,6 @@ class Public::SessionsController < Devise::SessionsController
   #   super
   # end
 
-  protected
-  
-  def customer_state
-    @customer = Customer.find_by(email: params[:customer][:email])
-    return if !@customer
-    if @customer.vaild_password?(params[:customer][:password]) && (@customer.is_deleted == true)
-      redirect_to new_customer_registration
-    end
-  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_in_params

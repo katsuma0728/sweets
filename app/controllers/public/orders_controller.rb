3540fb9b44810e7d497192f84e02ecfb86bfbb98
katsuma0728 <126679078+katsuma0728@users.noreply.github.com>
2023-04-19 20:54:01 +0000

    order-confiem

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -5,29 +5,33 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    if params[:order][:address_number] == "0"
+    if params[:order][:address_number] = "0"
       @order.shipping_address = current_customer.address
       @order.shipping_postal_code = current_customer.postal_code
       @order.shipping_name = current_customer.last_name + current_customer.first_name
-    elsif params[:order][:address_number] == "1"
+    elsif params[:order][:address_number] = "1"
       @address = Address.find(params[:order][:address_id])
       @order.shipping_address = @address.address
       @order.shipping_postal_code = @address.postal_code
       @order.shipping_name = @address.name
-    elsif params[:order][:address_number] == "2"
+    elsif params[:order][:address_number] = "2"
       @address = Address.new
       @order.shipping_address = params[:order][:shipping_address]
       @order.shipping_postal_code = params[:order][:shipping_postal_code]
       @order.shipping_name = params[:order][:shipping_name]
     end
     render :confirm
+    @order.customer_id = current_customer.id
+    @cart_items = CartItem.all(@order.customer_id)
+    @total = 0
+    @order.postage = 800
+    @order.billing_amount = @total + @order.postage
   end
 
   def create
     @order = Order.new(order_params)
-    @order.customer_id = current_customer.id
-    @order.save(order_params)
-    redirect_to confirm_order_path(params[:id])
+    @order.save
+    redirect_to complete_path
   end
 
   def complete
@@ -43,6 +47,6 @@ class Public::OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order).permit(:shipping_postal_code, :shipping_address, :shipping_name, :payment_method)
+    params.require(:order).permit( :cutomer_id, :shipping_postal_code, :shipping_address, :shipping_name, :payment_method, :billing_amount)
   end
 end
